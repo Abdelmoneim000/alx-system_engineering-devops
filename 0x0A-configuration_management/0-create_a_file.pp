ad6705460d15f4a3bcb264572b39ffdb7ad7e2c5
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-20 03:50:36 +0300

    finish task 0, creating a new file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# define the source file
+file { 'creating a new file':
+  ensure => present,                # Desired state (file exists)
+  path => '/home/abod/school',            # Path to the
+  owner  => 'www-data',              # Owner of the file
+  group  => 'www-data',              # Group of the file
+  mode   => '0744',              # Permissions (octal)
+  content => 'Welcome to my server!\n', # Content of the file
+}
