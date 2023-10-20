f7a5697464fbcbcab67db41d7d7d4e389384fbb0
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-20 04:54:12 +0300

    finish task 1. Download a package using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Installing a package using pip3
 package { 'flask':
-  ensure  => '2.1.0',                # Desired state (Installed)
-  command => 'pip3 install flask==2.1.0', # the command to use for installing
-  unless  => 'pip3 show flask | grep "Version: 2.1.0"'
+  ensure   => '2.1.0',                # Desired state (Installed)
+  provider => 'pip3' # module manager
 }
